23b16879a8a47573693964cbd83663accb0a8e55
AADITYA KUMAR <akgone38@gmail.com>
2022-04-11 15:58:56 +0530

    Add files via upload

---
+++ DataStrucQues/Stacks/dynamicStack.java
@@ -0,0 +1,20 @@
+public class dynamicStack extends stack {
+    public dynamicStack(int size){
+        super(size);
+    }
+    @Override
+    public void push(int val) throws Exception{
+        if(super.sizeOfArray()==super.size()){
+            int[] temp = new int[super.size()];
+            int idx= super.size()-1;
+            while(super.size()!=0){
+                temp[idx--] = super.pop();
+            }
+            super.initialize(super.sizeOfArray()*2);
+            for(int i=0;i<temp.length;i++){
+                super.push(temp[i]);
+            }
+        }
+        super.push(val);
+    }
+}
